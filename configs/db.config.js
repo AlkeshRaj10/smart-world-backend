824ab443e65f021e853f162506d998d528f0e290
AlkeshRaj10 <rajalkesh15@gmail.com>
2026-06-01 12:31:11 +0500

    Update MongoDB connection string in db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -5,7 +5,7 @@ const { DATABASE_URL } = require("./config");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(DATABASE_URL, {
+    const conn = await mongoose.connect("mongodb://asndev99:NwVahNSN77cEou29@ac-rzzev7n-shard-00-00.055namv.mongodb.net:27017,ac-rzzev7n-shard-00-01.055namv.mongodb.net:27017,ac-rzzev7n-shard-00-02.055namv.mongodb.net:27017/SmartWorld?ssl=true&replicaSet=atlas-yruwjw-shard-0&authSource=admin&retryWrites=true&w=majority&appName=asndevcluster", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
